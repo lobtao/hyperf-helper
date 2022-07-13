282307a32e1af2fb124a199c56ac9c88fb782300
lobtao <lobtao@gmail.com>
2022-07-13 21:19:32 +0800

    Update start.sh

--- publish/start.sh
+++ publish/start.sh
@@ -5,5 +5,5 @@
 # sudo -H -u www ./start.sh
 
 # php执行文件的路径最好使用绝对路径,不受系统其它配置环境的影响
-php ./bin/hyperf.php start
-# swoole-cli ./bin/hyperf.php start
+php -d swoole.use_shortname='Off' ./bin/hyperf.php start
+# swoole-cli -d swoole.use_shortname='Off' ./bin/hyperf.php start
