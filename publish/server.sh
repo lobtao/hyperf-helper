404638f9266cfa237dbe1300f33082a65a797c85
lobtao <lobtao@gmail.com>
2023-07-02 12:41:41 +0800

    Update server.sh

--- publish/server.sh
+++ publish/server.sh
@@ -18,7 +18,7 @@
 # sudo -H -u www ./server.sh status
 
 # php执行文件的路径最好使用绝对路径,不受系统其它配置环境的影响
-if [ $1 = "status" ]
+if [ "$1" = "status" ]
 then
     php -d swoole.use_shortname='Off' ./bin/hyperf.php server:status
 else
